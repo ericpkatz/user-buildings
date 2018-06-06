892f5269dd83e4bd56ddb5f4856e955542161b1e
Eric P. Katz <ericpkatz@gmail.com>
2018-06-05 20:25:34 -0400

    commit

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+console.log(process.env.DATABASE_URL);
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/my_db');
 
 const User = conn.define('user', {
